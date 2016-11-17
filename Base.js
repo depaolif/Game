b79ecf6ee544d72f1c8e3b45bc7e0e61af0da81e
depaolif <frederico.de.paoli@gmail.com>
2016-11-17 15:18:13 -0500

    update Base.js
    add descriptions of items, check if alive, spells

--- Base.js
+++ Base.js
@@ -10,6 +10,8 @@ function human(health,mana,name) {
   this.flying = false;
   this.inventory = {};
   this.mainWeapon = undefined
+  this.spellbook = {};
+  this.alive = true
 }
 
 function baseHuman() {
@@ -20,50 +22,165 @@ function baseHuman() {
 // Characters list
 var soldier = new human(150,0,"Soldier")
 var protagonist = new human(100,100,"Me")
+var archMage = new human(90,200,"ArchMage")
+
+// make a new creature
+var harpy = {
+  name: "Harpy",
+  health: 100,
+  mana: 100,
+  race: "Harpy",
+  type: "Humanoid",
+  flying: true,
+  inventory: [],
+  mainWeapon: claw = new weapon("Claw",30,"cutting",false),
+  alive: true
+}
+
+// Spells constructor
+
+function spell(name, manacost, type, cast, description) {
+  this.name = name;
+  this.category = "Spell";
+  this.type = type;
+  this.manacost = manacost;
+  this.cast = cast;
+  this.description = description
+}
+
+function spellCast(mana, manacost) {
+  mana -= manacost;
+  return mana
+}
+
+// Spells list
+
+var shatter = {
+  name: "Shatter",
+  category: "Spell",
+  type: "Evocation",
+  manaCost: 50,
+  level: 3,
+  cast: function(target) {
+    spellCast()
+    console.log(`${target.name}'s weapon has been shattered.`)
+  },
+  varname: "shatter"
+}
+
+
 
 
 
 
 // Weapons constructor
-function weapon(name, attack,type) {
+function weapon(name, attack,type, isRanged, description) {
   this.name = name;
   this.category = "Weapon";
   this.attack = attack;
   this.type = type;
-  this.varname = name.replace(/[\'\ \"]/g,'')
+  this.isRanged = isRanged;
+  this.description = description;
+  
+  // Make varname match the actual variable name (i.e. undercase first letter, no ' " _ or spaces)
+  this.varname = name.replace(/[\'\ \"\_]/g,'');
+  this.varname = this.varname.substr(0,1).toLowerCase() + this.varname.substr(1)
 }
 
 // Weapons list
-var knife = new weapon("Knife",20,"cutting")
-var sentosScepter = new weapon("Sento\'s Scepter",250,"arcane")
+var knife = new weapon("Knife",20,"cutting",false,"A simple, metal knife used to shred wood.")
+
+var sentosScepter = new weapon("Sento\'s Scepter",250,"arcane",true,"The last scepter used by the ArchMage Sento. It fires a beam of light from the ruby shard embedded in the tip.")
+
+var bow = new weapon("Bow",40,"piercing",true,"A shortbow used for hunting.")
+
+var sword = new weapon("Sword",50,"cutting",false,"A bronze sword typical of army footsoldiers.")
+
+// Initialize inventories
+soldier.mainWeapon = sword
+archMage.mainWeapon = sentosScepter
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 // Inventory actions
 function pickUp(subject, object) {
-  subject.inventory[object.varname] = object
+  subject.inventory[object.varname] = object;
+  console.log(`${subject.name} has picked up a ${object.name}.`)
 }
+
 function equip(subject,object) {
   if (subject.inventory.hasOwnProperty(object.varname) === true) {
     subject.mainWeapon = object
     delete subject.inventory[object.varname]
+    console.log(`${subject.name} has equipped a ${object.name}.`)
+  } else {
+    console.log(`${subject.name} does not have a ${object.name}.`)
   }
 }
+
+// Spellbook actions
+function learnSpell(subject, spell) {
+  subject.spellbook[spell.varname] = spell;
+  console.log(`${subject.name} has learned the ${spell.type} spell, ${spell.name}.`);
+}
+
+
+
+// Read an item's description
+function read(item) {
+  console.log(`${item.name}: ${item.description}`)
+}
+
+
 
 
 // Attack actions
 function attack(subject, target) {
+  // do damage
   if (subject.mainWeapon !== undefined) {
+    // check if the target is ranged and the weapon is ranged
+    if (target.flying !== true || (target.flying === true && subject.mainWeapon.isRanged === true)) {  
     var damage = subject.mainWeapon.attack
-    var damageType = subject.mainWeapon.type
-    target.health -= damage
-    console.log(`You've attacked ${target.name} for ${damage} ${damageType} damage!`)
+      var damageType = subject.mainWeapon.type
+      target.health -= damage
+      console.log(`${subject.name} has attacked ${target.name} for ${damage} ${damageType} damage!`)
+      var killed = isDead(target)
+      
+      // else if can't reach flying
+      } else {
+        console.log(`${subject.name} can't reach ${target.name} with ${subject.name}'s ${subject.mainWeapon.name}.`)
+      }
+    
+    // else if no weapon equipped     
   } else {
     console.log("You don't have a weapon equipped!")
   }
+    
 }
 
+// check to see if character is dead
+function isDead(target) {
+  if (target.health <= 0) {
+    console.log(`${target.name} has died.`)
+    target.alive = false
+    return true
+  } else {
+    return false
+  }
+}
 
 
 
